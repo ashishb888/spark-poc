abf9cdd82ad535b6337a872e0e6b1b1fb450c031
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-08-25 22:14:26 +0530

    Initial commit

---
+++ spark-gs/src/main/java/poc/spark/TestApp.java
@@ -0,0 +1,9 @@
+package poc.spark;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ spark-gs/src/test/java/poc/spark/Test.java
@@ -0,0 +1,9 @@
+package poc.spark;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	}
+
+}
