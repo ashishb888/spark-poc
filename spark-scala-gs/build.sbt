ad4917efb59f3de016577ca91d010a564d89434d
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-09-28 18:38:21 +0530

    Added CsvApp object

---
+++ spark-scala-gs/build.sbt
@@ -0,0 +1,5 @@
+name := "spark-scala-gs"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
---
+++ spark-scala-gs/src/main/scala/poc/spark/main/CsvApp.scala
@@ -0,0 +1,7 @@
+package poc.spark.main
+
+object CsvApp {
+  def main(args: Array[String]): Unit = {
+    println("Hello Spark")
+  }
+}
