9d0b9952bf360270f173f7f6819a838d5e0a9870
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-09-28 20:56:02 +0530

    Mapped Scala version wih Spark version. Ran First Spark app successfully

--- spark-scala-gs/build.sbt
+++ spark-scala-gs/build.sbt
@@ -2,4 +2,11 @@ name := "spark-scala-gs"
 
 version := "0.1"
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.11.12"
+
+val sparkVersion = "2.4.3"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+)
--- spark-scala-gs/src/main/scala/poc/spark/main/CsvApp.scala
+++ spark-scala-gs/src/main/scala/poc/spark/main/CsvApp.scala
@@ -1,7 +1,20 @@
 package poc.spark.main
 
+import org.apache.spark.sql.SparkSession
+
 object CsvApp {
-  def main(args: Array[String]): Unit = {
-    println("Hello Spark")
+  def readCsv(): Unit = {
+    val logFile = "/var/tmp/test.csv"
+    val spark = SparkSession.builder.appName("Csv App").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
   }
+
+  def main(args: Array[String]): Unit = {
+    readCsv();
+  }
+
 }
