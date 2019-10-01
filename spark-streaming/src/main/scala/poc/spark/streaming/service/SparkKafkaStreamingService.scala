ba3a2de6835a2671d543ed2f34e4423be4afd6ad
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-10-01 19:37:17 +0530

    Reading from Kafka, groupBy and then aggregate

--- spark-streaming/src/main/scala/poc/spark/streaming/service/SparkKafkaStreamingService.scala
+++ spark-streaming/src/main/scala/poc/spark/streaming/service/SparkKafkaStreamingService.scala
@@ -4,11 +4,64 @@ import java.util.UUID
 
 import org.apache.spark.sql.SparkSession
 
-
 object SparkKafkaStreamingService {
 
   def start(topic: String, brokers: String): Unit = {
-    inOutKafka(topic, brokers)
+    // inOutKafka(topic, brokers)
+    readJson(topic, brokers)
+  }
+
+  def readJson(topic: String, brokers: String): Unit = {
+    println("readJson service")
+
+    val spark = SparkSession.builder.appName("Spark Streaming App").getOrCreate()
+
+    import spark.implicits._
+    import org.apache.spark.sql.functions.{get_json_object, json_tuple, max}
+
+    val inputStream = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", brokers)
+      .option("subscribe", topic)
+      .option("startingOffsets", "earliest")
+      // .option("minPartitions", "10")
+      .option("failOnDataLoss", "true")
+      .load()
+    inputStream.printSchema
+
+    //    val groupBySeries = inputStream
+    //      .select(get_json_object(($"value").cast("string"), "$.SERIES").alias("SERIES"))
+    //      .groupBy($"SERIES")
+    //      .count()
+
+    val groupByTimestamp = inputStream
+      .select(get_json_object(($"value").cast("string"), "$.TIMESTAMP").alias("TIMESTAMP"), get_json_object(($"value").cast("string"), "$.TOTTRDVAL").cast("double").alias("TOTTRDVAL"))
+      .groupBy($"TIMESTAMP")
+      //.count()
+      .agg(max("TOTTRDVAL").alias("MAX TOTTRDVAL"))
+
+    import org.apache.spark.sql.streaming.ProcessingTime
+
+    //    val seriesQuery =
+    //      groupBySeries
+    //        .writeStream
+    //        .format("console")
+    //        .outputMode("complete")
+    //        .trigger(ProcessingTime("20 seconds"))
+    //        .start()
+    //
+    //    seriesQuery.awaitTermination()
+
+    val timestampQuery =
+      groupByTimestamp
+        .writeStream
+        .format("console")
+        .outputMode("complete")
+        .trigger(ProcessingTime("20 seconds"))
+        .start()
+
+    timestampQuery.awaitTermination()
   }
 
   def inOutKafka(topic: String, brokers: String): Unit = {
